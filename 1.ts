47911f84ba2ce56c86937a7b9a74f33008e63549
Bsurveillant <z123xcv@foxmail.com>
2022-02-18 19:41:15 +0800

    2022年2月18日

---
+++ 1.ts
@@ -0,0 +1,36 @@
+
+/*class Stack{
+    items: number[] = [];
+    push(element: number) {
+        this.items.push(element);
+    }
+    pop(): number{
+        return this.items.pop();
+    }
+}
+
+let stack = new Stack();
+// console.log(stack.push);
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.items);
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.items);
+*/
+
+function one(){
+    console.log(1)
+    function two(){
+        console.log(2)
+        function three(){
+            console.log(3)
+        }
+        three()
+    }
+    two()
+}
+
+one()
---
+++ 2.js
@@ -0,0 +1,6 @@
+var a = 1;
+var b = 2;
+var c = {
+    name: 'zhufeng',
+    age: 10
+}
